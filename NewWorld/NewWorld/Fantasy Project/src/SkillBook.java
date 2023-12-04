51dd3f0d2d6d31dcc08ad22cc148e98d96adfc53
michaelwang18 <145706779+michaelwang18@users.noreply.github.com>
2023-12-04 11:45:47 -0500

    Add files via upload

---
+++ NewWorld/NewWorld/Fantasy Project/src/SkillBook.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SkillBook {
+    private SkillBook(){}
+
+
+    public static String[] fireball(){
+        String[] skill = {"Fireball", "1.50", "Single Target"};
+
+        return skill;
+    }
+    public static String[] vicmoc(){
+        String[] skill = {"Vicious Mockery", ".15", "Mass Attack"};
+
+        return skill;
+    }
+    public static String[] slash(){
+        String[] skill = {"Slash", "1", "Single Target"};
+
+        return skill;
+    }
+    public static String[] airSlash(){
+        String[] skill = {"Air Slash", "1", "Mass Attack"};
+
+        return skill;
+    }
+    public static String[] shieldBash(){
+        String[] skill = {"Shield Bash", "1.25", "Single Target"};
+
+        return skill;
+    }
+    public static String[] rockThrow(){
+        String[] skill = {"Rock Throw", ".75", "Single Target"};
+
+        return skill;
+    }
+    public static String[] bite(){
+        String[] skill = {"Bite", ".75", "Single Target"};
+
+        return skill;
+    }
+    public static String[] scratch(){
+        String[] skill = {"Scratch", ".5", "Mass Attack"};
+
+        return skill;
+    }
+    public static String[] clubBash(){
+        String[] skill = {"Club Bash", "1.75", "Single Target"};
+
+        return skill;
+    }
+    public static String[] arrowShot(){
+        String[] skill = {"Arrow Shot", "1.5", "Single Target"};
+        return skill;
+    }
+    public static String[] arrowVolley(){
+        String[] skill = {"Arrow Volley", ".75", "Mass Attack"};
+        return skill;
+    }
+    public static String[] fireBreath(){
+        String[] skill = {"Fire Breath", "1.25", "Mass Attack"};
+        return skill;
+    }
+    public static String[] thunder(){
+        String[] skill = {"Thunder", "1.35", "Mass Attack"};
+        return skill;
+    }
+
+    public static String[] obliterate(){
+        String[] skill = {"Obliterate", "500", "Mass Attack"};
+        return skill;
+    }
+
+}
