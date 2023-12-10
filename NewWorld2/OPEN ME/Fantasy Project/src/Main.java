f2debc8aca48900dc6a445bba597c88f9af985fc
michaelwang18 <145706779+michaelwang18@users.noreply.github.com>
2023-12-10 18:24:31 -0500

    Add files via upload

---
+++ NewWorld2/OPEN ME/Fantasy Project/src/Main.java
@@ -0,0 +1,16 @@
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        Game game = new Game();
+        //System.out.println("Enter Int");
+        //System.out.println(Utility.tryInput(scan.nextLine(),5));
+
+    }
+}
+
+//https://www.w3schools.com/c/c_arrays.php#:~:text=Arrays%20are%20used%20to%20store,followed%20by%20square%20brackets%20%5B%5D.
+//w3schools referenced for C arrays and arraylists
