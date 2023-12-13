fb7a633ae42de3574aba42672621af8cf6eb06bf
michaelwang18 <145706779+michaelwang18@users.noreply.github.com>
2023-12-13 17:26:16 -0500

    Add files via upload

---
+++ NewWorld2/OPEN ME/Fantasy Project/src/SkillBook.java
@@ -0,0 +1,186 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.SplittableRandom;
+
+public class SkillBook {
+    private SkillBook() {
+    }
+
+
+
+
+    // MAGIC SKILLS
+
+    public static String[] fireball() {
+        String[] skill = {"Fireball", "1.75", "Single Target", "1"}; //Name, dmg Scale, targeting, Hits
+        return skill;
+    }
+
+    public static String[] fireBreath() {
+        String[] skill = {"Fire Breath", "1.25", "Mass Attack", "1"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] icicleBarrage() {
+        String[] skill = {"Icicle Barrage", ".85", "Mass Attack", "4"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] thunder() {
+        String[] skill = {"Thunder", "1.5", "Mass Attack", "2"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] magicMissiles() {
+        String[] skill = {"Magic Missiles", ".60", "Single Target", "5"}; //Name, dmg Scale, targeting, Hits
+        return skill;
+    }
+
+    public static String[] spiritBomb() {
+        String[] skill = {"Spirit Bomb", "1.25", "Mass Attack", "1"}; //Name, dmg Scale, targeting, Hits
+        return skill;
+    }
+
+    public static String[] magicBolt() {
+        String[] skill = {"Magic Bolt", "1.50", "Single Target", "1"}; //Name, dmg Scale, targeting, Hits
+        return skill;
+    }
+
+    public static String[] thornBurst() {
+        String[] skill = {"Magic Bolt", "2.5", "Mass Attack", "1"}; //Name, dmg Scale, targeting, Hits
+        return skill;
+    }
+
+    public static String[] mysticLeaves() {
+        String[] skill = {"Mystic Leaves", ".9", "Mass Attack", "3"}; //Name, dmg Scale, targeting, Hits
+        return skill;
+    }
+
+
+
+    // SWORD SKILLS
+
+    public static String[] slash() {
+        String[] skill = {"Slash", "1.75", "Single Target", "1"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] twinslash() {
+        String[] skill = {"Twin Slash", "1", "Single Target", "2"}; //Name, dmg Scale, targeting, hits
+        return skill;
+    }
+
+    public static String[] airSlash() {
+        String[] skill = {"Air Slash", "1.5", "Mass Attack", "1"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] shieldBash() {
+        String[] skill = {"Shield Bash", "2.25", "Single Target", "1"};//Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] swordSweep() {
+        String[] skill = {"Sword Sweep", "2", "Mass Attack", "1"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] swordThrust() {
+        String[] skill = {"Sword Thrust", "2.5", "Single Target", "1"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] tripleSlash() {
+        String[] skill = {"Triple Slash", "1.75", "Single Target", "3"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] swordCleave() {
+        String[] skill = {"Sword Cleave", "1.65", "Mass Attack", "2"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    //ARCHER SKILLS
+
+    public static String[] arrowShot() {
+        String[] skill = {"Arrow Shot", "1.75", "Single Target", "1"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] tripleShot() {
+        String[] skill = {"Triple Shot", ".75", "Single Target", "3"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] arrowVolley() {
+        String[] skill = {"Arrow Volley", ".85", "Mass Attack", "2"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] doublebarel() {
+        String[] skill = {"Double Barrel", "2", "Single Target", "2"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] revolver() {
+        String[] skill = {"Revolver", ".5", "Single Target", "6"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] grenade() {
+        String[] skill = {"Grenade", "1.5", "Mass Attack", "1"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] arrowRain() {
+        String[] skill = {"Double Barrel", "2", "Single Target", "2"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+
+
+
+
+    //MISC SKILLS
+    public static String[] vicmoc() {
+        String[] skill = {"Vicious Mockery", ".15", "Mass Attack", "1"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+
+    public static String[] rockThrow() {
+        String[] skill = {"Rocks Throw", ".75", "Single Target", "4"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] bite() {
+        String[] skill = {"Bite", ".75", "Single Target", "1"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] scratch() {
+        String[] skill = {"Scratch", ".75", "Mass Attack", "1"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] clubBash() {
+        String[] skill = {"Club Bash", "1.65", "Single Target", "1"};//Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] obliterate() {
+        String[] skill = {"Obliterate", "500", "Mass Attack", "2"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] machineGun() {
+        String[] skill = {"Machine Gun", ".1", "Single Target", "100"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+    public static String[] gasterBlasters() {
+        String[] skill = {"Gaster Blaster", ".35", "Mass Attack", "3"}; //Name, dmg Scale, targeting
+        return skill;
+    }
+
+}
